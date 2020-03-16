e74f7ab70bd73b93e815cf3f7bd42a510ca9ae7c
Daniel Ilan <daniel.ilan222@gmail.com>
2020-03-17 00:51:33 +0200

    loads from editTemplate page changed some JS opyion to select animation changed some values on the layout.html

--- videoProject/videoProject/static/scripts/JavaScript.js
+++ videoProject/videoProject/static/scripts/JavaScript.js
@@ -1,27 +1,46 @@
 
+function changeAnimation(event) {
+    const main_animation = document.querySelector('#mainAnimation');
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+        main_animation.load(event.currentTarget.src)
+    };
+    xhttp.open("POST", "/editTemplate", true);
+    xhttp.send(event.currentTarget.src);
+
+}
 
 
-$(".sideNavUl").click(
-    function (event) {
-        $(this).find("a").forEach(
-            function () {
-                if ($(this).find("a").hasClass('active')) {
-                    $(this).removeClass('text-white');
-                    $(this).find("svg").removeClass('svgFill');
+$(document).ready(function(){
+    $(".sideNavUl").click(
+        function (event) {
+            $(this).find("a").forEach(
+                function () {
+                    if ($(this).find("a").hasClass('active')) {
+                        $(this).removeClass('text-white');
+                        $(this).find("svg").removeClass('svgFill');
+                    }
+
                 }
+            )
+        });
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
 
-            }
-        )
-    });
+    const nav_links = $("a[href='${page}']");
+    nav_links.children().removeClass('svgFill');
+    nav_links.children().addClass('svgFillActive');
+    nav_links.addClass('active');
+    nav_links.removeClass('text-white');
 
-var path = window.location.pathname;
-var page = path.split("/").pop();
+    const animation_templates = $('.animation-template');
 
-$(`a[href='${page}']`).children().removeClass('svgFill');
-$(`a[href='${page}']`).children().addClass('svgFillActive');
+    animation_templates.on('click', changeAnimation);
 
-$(`a[href='${page}']`).addClass('active');
-$(`a[href='${page}']`).removeClass('text-white');
+    // animation_templates.forEach(function () {
+    //     $(this).addEventListener('click', changeAnimation(this, main_animation))
+    // });
+});
 
 
 
--- videoProject/videoProject/views.py
+++ videoProject/videoProject/views.py
@@ -7,18 +7,18 @@ from videoProject import app
 import os
 from datetime import datetime
 from os import environ
-
-from flask import render_template, request, make_response
+import time
+from flask import render_template, request, make_response, redirect, url_for, send_file
 from tgs import Color
 from tgs import objects
 from tgs.parsers.tgs import parse_tgs
-from tgs.utils import script
 from tgs import exporters
 
 
+file = "static/content/temp2.json"
 
 
-@app.route('/')
+
 @app.route('/home')
 def home():
     """Renders the home page."""
@@ -26,6 +26,7 @@ def home():
         'index.html',
         title='Home Page',
         year=datetime.now().year,
+        file=file
     )
 
 
@@ -58,7 +59,8 @@ def editContent():
         'editContent.html',
         title='תוכן',
         year=datetime.now().year,
-        message=''
+        message='',
+        file=file
     )
 
 
@@ -69,37 +71,63 @@ def editColor():
         'editColor.html',
         title='צבע',
         year=datetime.now().year,
-        message=''
+        message='',
+        file=file
     )
 
 
-@app.route('/editTemplate')
+@app.route("/")
+@app.route('/editTemplate', methods=['POST', 'GET'])
 def editTemplate():
+    """
+    gets an ajax request and changes the json file attached to the lottie-player
+    todo: this is not secure now and needs to be written correctly
+    :return:
+    """
+    print("I am here")
     """Renders the about page."""
+    if request.method == 'POST':
+        a = request.data
+        change_animation(a.decode("utf-8"))
+        print(a)
     return render_template(
         'editTemplate.html',
         title='תבנית',
         year=datetime.now().year,
-        message=''
+        message='',
+        file=file
     )
 
 
 @app.route('/success', methods=['POST', 'GET'])
 def print_data():
+    """
+    this will be used to change the colors for now it's here
+    :return:
+    """
     os.path.join(os.path.dirname(os.path.abspath(__file__)))
     if request.method == 'POST':
+        global file
         result = request.form['username'].split(",")
         result = [float(i) for i in result]
-        print(result)
         an = parse_tgs("static/content/temp2.json")
         layers = an.layers
-        print(layers)
         for layer in layers:
             layer.effects = [objects.effects.FillEffect(color=Color(result[0], result[1], result[2]), opacity=1)]
-            print(layer)
-        exporters.export_lottie(an, "static/content/temp2.json")
-        # script.script_main(an, path="/tmp", formats=["json"])
-        return render_template("index.html", result=result)
+
+        new_name = "temp"+str(int(time.time()))+".json"
+        exporters.export_lottie(an, "static/content/"+new_name)
+        new_json = "static/content/"+new_name
+        if file[-6:-9:-1].isdigit():
+            os.remove(file)
+        file = new_json
+        return render_template("index.html", file=file)
+
+
+def change_animation(path):
+    global file
+    file = path
+    return render_template("editTemplate.html", file=file)
 
 
 if __name__ == '__main__':
