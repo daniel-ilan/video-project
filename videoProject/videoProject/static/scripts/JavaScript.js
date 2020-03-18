942d72c6d305d3b11c0e1606ff3bf75032c6a516
Reuven-deri <rubider@hotmail.com>
2020-03-18 20:50:23 +0200

    change after daniel

--- videoProject/videoProject/static/scripts/JavaScript.js
+++ videoProject/videoProject/static/scripts/JavaScript.js
@@ -1,3 +1,33 @@
+$(document).ready(function(){
+    /*          ----            side-nav change color           ----            */
+    $(".sideNavUl").click(
+        function (event) {
+            $(this).find("a").forEach(
+                function () {
+                    if ($(this).find("a").hasClass('active')) {
+                        $(this).removeClass('text-white');
+                        $(this).find("svg").removeClass('svgFill');
+                    }
+                }
+            )
+        });
+    var path = window.location.pathname;
+    var page = path.split("/").pop();
+
+    $(`a[href='${page}']`).children().removeClass('svgFill');
+    $(`a[href='${page}']`).children().addClass('svgFillActive');
+
+    $(`a[href='${page}']`).addClass('active');
+    $(`a[href='${page}']`).removeClass('text-white');
+
+
+    const animation_templates = $('.animation-template');
+    animation_templates.on('click', changeAnimation);
+
+    // animation_templates.forEach(function () {
+    //     $(this).addEventListener('click', changeAnimation(this, main_animation))
+    // });
+});
 
 function changeAnimation(event) {
     const main_animation = document.querySelector('#mainAnimation');
@@ -9,38 +39,6 @@ function changeAnimation(event) {
     xhttp.send(event.currentTarget.src);
 
 }
-
-
-$(document).ready(function(){
-    $(".sideNavUl").click(
-        function (event) {
-            $(this).find("a").forEach(
-                function () {
-                    if ($(this).find("a").hasClass('active')) {
-                        $(this).removeClass('text-white');
-                        $(this).find("svg").removeClass('svgFill');
-                    }
-
-                }
-            )
-        });
-    var path = window.location.pathname;
-    var page = path.split("/").pop();
-
-    const nav_links = $("a[href='${page}']");
-    nav_links.children().removeClass('svgFill');
-    nav_links.children().addClass('svgFillActive');
-    nav_links.addClass('active');
-    nav_links.removeClass('text-white');
-
-    const animation_templates = $('.animation-template');
-
-    animation_templates.on('click', changeAnimation);
-
-    // animation_templates.forEach(function () {
-    //     $(this).addEventListener('click', changeAnimation(this, main_animation))
-    // });
-});
 
 
 
