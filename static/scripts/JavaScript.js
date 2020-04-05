ef1a0ed18ea232344ba64564fde4ea1bf91d5bf4
Reuven-deri <rubider@hotmail.com>
2020-04-05 19:21:02 +0300

    change after daniel + new side ui

--- static/scripts/JavaScript.js
+++ static/scripts/JavaScript.js
@@ -1,45 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function () {
     /*          ----            side-nav change color           ----            */
-    $(".sideNavUl").click(
-        function (event) {
-            $(this).find("a").forEach(
-                function () {
-                    if ($(this).find("a").hasClass('active')) {
-                        $(this).removeClass('text-white');
-                        $(this).find("svg").removeClass('svgFill');
-                    }
-                }
-            )
-        });
     var path = window.location.pathname;
     var page = path.split("/").pop();
-
     $(`a[href='${page}']`).children().removeClass('svgFill');
     $(`a[href='${page}']`).children().addClass('svgFillActive');
 
     $(`a[href='${page}']`).addClass('active');
-    $(`a[href='${page}']`).removeClass('text-white');
-    const animation_templates = $('.tinyLottie');
+    $(`a[href='${page}']`).parent().addClass('activeNav');
+
+    var indexActive = 0;
+    var counter = 0;
+    $(".sidebar li").each(function () {
+        counter++;
+        if ($(this).hasClass('activeNav')) {
+            indexActive = counter;
+        }
+    });
+    $(".sidebar li:nth-child(" + (indexActive - 1).toString() + ")").addClass('upNavUI');
+    $(".sidebar li:nth-child(" + (indexActive + 1).toString() + ")").addClass('downNavUI');
+
+
+    /*          ----            change animation - editTemplate           ----            */
+    const animation_templates = $('.tinyLottiePlayer');
     animation_templates.on('click', changeAnimation);
-
-
-    // animation_templates.forEach(function () {
-    //     $(this).addEventListener('click', changeAnimation(this, main_animation))
-    // });
 });
 
 function changeAnimation(event) {
     const main_animation = document.querySelector('#mainAnimation');
     const xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = function() {
+    xhttp.onreadystatechange = function () {
         main_animation.load(event.currentTarget.src)
     };
     xhttp.open("POST", "/editTemplate", true);
     xhttp.send(event.currentTarget.src);
 
 }
-
-
 
 
 
