8221dfd221c1477dcbd7f56f9f2f782d0915412e
Reuven-Deri <37385453+Reuven-Deri@users.noreply.github.com>
2020-04-15 18:52:33 +0300

    Accdb db rubi (#9)
    * Merge branch 'master' of C:\Users\rubi\WebstormProjects\video-project with conflicts.
    
    * change after daniel
    
    * new user def
    
    * new proj def
    
    * new proj def
    
    * try db with Microsoft Access
    
    * try db with Microsoft Access
    
    * try db with Microsoft Access
    
    * try db with Microsoft Access
    
    * db with Microsoft Access
    
    DEF: new proj , new user, add color,check palte

--- application.py
+++ application.py
@@ -9,6 +9,7 @@ from datetime import datetime
 from flask import Flask
 from flask import render_template, request
 from matplotlib import colors
+import pyodbc
 from tgs import exporters, objects
 from tgs.parsers.tgs import parse_tgs
 
@@ -73,9 +74,71 @@ def get_anim_props(anim):
     return anim_props
 
 changing_path = "static/content/temp1_anim2.json"
+
+
+def create_conn():
+    connStr = (
+        r"DRIVER={Microsoft Access Driver (*.mdb, *.accdb)};"
+        r"DBQ=static\db\db.accdb;"
+    )
+    conn = pyodbc.connect(connStr)
+    cursor = conn.cursor()
+    return (conn, cursor)
+
+
+def get_row(query:str):
+    conn, cursor= create_conn()
+    cursor.execute(query)
+    data = len(cursor.fetchall())
+    cursor.close()
+    return data
+
+
+def update_query(query:str):
+    conn, cursor= create_conn()
+    cursor.execute(query)
+    conn.commit()
+    cursor.close()
+
+def select_one_query(query:str):
+    conn, cursor= create_conn()
+    cursor.execute(query)
+    query_data = cursor.fetchone()
+    cursor.close()
+    return query_data
+
+
+def select_many_query(query:str, some:str):
+    conn, cursor= create_conn()
+    cursor.execute(query)
+    query_data = cursor.fetchmany(some)
+    cursor.close()
+    return query_data
+
+def select_all_query(query:str):
+    conn, cursor= create_conn()
+    cursor.execute(query)
+    query_data = cursor.fetchall()
+    cursor.close()
+    return query_data
+
+
+def get_all_animations():
+    query = f"SELECT [animation_url],[animation_name] FROM animations"
+    return select_all_query(query)
+
 an = readable(changing_path)
 anim_properties = get_anim_props(an)
+# lottiePlayersArray = [["temp1_anim1.json", "שקיפות"], ["temp1_anim2.json", "בהדרגה מימין"],
+#                       ["temp1_anim3.json", "מצד ימין"], ["temp1_anim4.json", "שלום"], ["temp2_anim1.json", "שקיפות"],
+#                       ["temp2_anim2.json", "בהדרגה מימין"], ["temp2_anim3.json", "מצד ימין"],
+#                       ["temp2_anim4.json", "שלום"]]
 
+lottiePlayersArray = get_all_animations()
+    #get_all_animations()
+lottiePlayersArrayPath = "../static/content/"
+
+colorsArray= []
 myLoAr = ["../static/content/temp1_anim1.json","../static/content/temp1_anim1.json"]
 lottiePlayersArray = [["temp1_anim1.json","אנימציה 1"],
                       ["temp1_anim2.json","אנימציה 2"],
@@ -87,6 +150,7 @@ lottiePlayersArray = [["temp1_anim1.json","אנימציה 1"],
                       ["temp2_anim4.json", "אנימציה 8"]
                       ]
 lottiePlayersArrayPath ="../static/content/"
+
 
 @application.route('/home')
 def home():
@@ -109,15 +173,125 @@ def contact():
         message='Your contact page.'
     )
 
-
-@application.route('/about')
+@application.route('/about', methods=['POST', 'GET'])
 def about():
+    global person_name
+    global person_last_name
+    global email
+    global password
+    if request.method == 'POST':
+        #new user
+        person_name = request.form['person_name']
+        person_last_name = request.form['person_last_name']
+        email = request.form['email']
+        password = request.form['password']
+        image = None  #request.form['image']
+        create_new_user(person_name, person_last_name, email, password, image)
+        # get user info
+        # dataFromDB = get_user(str(request.form['existUserEmail']), str(request.form['existUserPass']))
+        # print(dataFromDB)
+        # person_name = dataFromDB[1]
+        # person_last_name = dataFromDB[2]
+        # email = dataFromDB[3]
+        # password = dataFromDB[4]
+    else:
+         person_name=""
+         person_last_name=""
+         email=""
+         password=""
     """Renders the about page."""
     return render_template(
         'about.html',
         title='About',
         year=datetime.now().year,
-        message='Your application description page.'
+        message='Your application description page.',
+        person_name= person_name,
+        person_last_name=person_last_name,
+        email=email,
+        password=password
+    )
+
+
+@application.route('/newProject', methods=['POST', 'GET'])
+def newProject():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'submit_id':
+            # get user info
+            userID = request.form['userID']
+            update_project_last_update("2")
+            data = get_project_info(userID)
+            print(data)
+        else:
+            # new user
+            userID = int(request.form['project_owner'])
+            project_name = request.form['project_name']
+            project_image = None  # request.form['project_image']
+            create_new_project(userID, project_name, project_image)
+
+    """Renders the contact page."""
+    return render_template(
+        'newProject.html',
+        title='newProject',
+        year=datetime.now().year,
+        message='Your contact page.',
+        username = "xxxx"
+    )
+
+@application.route("/")
+@application.route('/homePage', methods=['POST', 'GET'])
+def homePage():
+    global alertM
+    global color1
+    global color2
+    global color3
+    global color4
+    global palteName
+    palteName=""
+    colors_data =["#000000"]
+    # color1 = color2 = color3 = color4 = "#000000"
+
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'submit_new_Color':
+            color = request.form['add_color']
+            kind = request.form['kind']
+            create_color(color, kind)
+        elif request.form['submit_button'] == 'submit_search_palte_id':
+            search_palte_id = request.form['search_palte_id']
+            data = get_palte(search_palte_id)
+            palteName = data[2]
+            colors_data = get_colors_by_plate(search_palte_id)
+            # color1 = colors_data[0][0]
+            # color2 = colors_data[1][0]
+            # color3 = colors_data[2][0]
+            # color4 = colors_data[3][0]
+
+            print(colors_data)
+        else:
+            if request.form['existUserEmail'] != '' and request.form['existUserPass'] != '':
+                dataFromDB = get_user(str(request.form['existUserEmail']), str(request.form['existUserPass']))
+                print(dataFromDB)
+                if dataFromDB[0] is True and dataFromDB[1] is False:
+                    alertM= "סיסמא שגויה"
+                elif dataFromDB[0] is True and dataFromDB[1] is True:
+                    alertM= "התחברות הצליחה"
+                else:
+                    alertM= "שם משתמש או סימא שגויים"
+    else:
+        alertM=""
+    """Renders the contact page."""
+    return render_template(
+        'homePage.html',
+        title='homePage',
+        year=datetime.now().year,
+        message='Your contact page.',
+        alertMessage=alertM,
+        # color1=color1,
+        # color2=color2,
+        # color3=color3,
+        # color4=color4,
+        my_colors= colors_data,
+        palte_name=palteName
+
     )
 
 
@@ -197,9 +371,9 @@ def editColor():
         message='',
         anim_path=changing_path,
         mainColor=main_color,
+
         outlineColor=outline_color,
         opacity=prim_opacity
-
     )
 
 
@@ -228,9 +402,11 @@ def change_color(color, outline_color, opacity):
     anim_properties['shapes']['secondary']['color'] = list(correct_outline_color[0:3] + (1,))
 
 
-@application.route("/")
+# @application.route("/")
 @application.route('/editTemplate', methods=['POST', 'GET'])
 def editTemplate():
+    colorsArray = get_colors_by_plate("1")
+    print(colorsArray)
     global changing_path
     """
     gets an ajax request and changes the json file attached to the lottie-player
@@ -249,8 +425,117 @@ def editTemplate():
         message='',
         anim_path=changing_path,
         lottiePlayersArray=lottiePlayersArray,
-        lottiePlayersArrayPath=lottiePlayersArrayPath
+        lottiePlayersArrayPath=lottiePlayersArrayPath,
+        colorsArray=colorsArray
+
     )
+
+
+
+
+
+def create_new_user(person_name: str, person_last_name: str, email: str, password: str, image: str = None):
+    image = f"'{image}'" if image else 'null'
+    person_name = email.strip()
+    person_last_name = password.strip()
+    email = email.strip()
+    password = password.strip()
+    query = f"INSERT INTO users ([person_name] ,[person_last_name],[email] ,[password] ,[image]) VALUES('{person_name}','{person_last_name}','{email}','{password}',{image});"
+    update_query(query)
+
+def get_user(email: str, password: str):
+    email = email.strip()
+    password = password.strip()
+    exsistUser = False
+    match = False
+    query = f"SELECT COUNT (*) FROM users WHERE email= '{email}';"
+    if get_row(query) == 1:
+        exsistUser = True
+        query = f"SELECT * FROM users WHERE email= '{email}' AND password='{password}';"
+        if get_row(query) == 1:
+            match = True
+    return (exsistUser, match)
+
+
+def create_new_project(user_id: int, project_name: str, image: str = None):
+    image = f"'{image}'" if image else 'null'
+    query = f"INSERT INTO projects([user_id] ,[project_name],[image]) VALUES({user_id},'{project_name}', {image});"
+    update_query(query)
+
+
+def get_project_info(user_id: str):
+    user_id = int(user_id.strip())
+    query = f"SELECT * FROM projects WHERE user_id={user_id};"
+    return select_all_query(query)
+
+
+def update_project_name(project_id: str, new_name:str):
+    project_id = int(project_id.strip())
+    query = f"UPDATE projects SET project_name='{new_name}' WHERE project_id={project_id}  ;"
+    update_query(query)
+
+
+def update_project_image(project_id: str, new_img:str):
+    project_id = int(project_id.strip())
+    query = f"UPDATE projects SET image='{new_img}' WHERE project_id={project_id}  ;"
+    update_query(query)
+
+
+def update_project_last_update(project_id: str):
+    project_id = int(project_id.strip())
+    query = f"UPDATE projects SET last_update= Date() WHERE project_id={project_id}  ;"
+    update_query(query)
+
+
+def update_project_status(project_id: str, status:str):
+    status = status.strip()
+    project_id = int(project_id.strip())
+    query = f"UPDATE projects SET status='{status}' WHERE project_id={project_id}  ;"
+    update_query(query)
+
+
+def create_color(hex:str, kind: str, palte_id: str):
+    hex = hex.strip()
+    kind = kind.strip()
+    palte_id = int(palte_id.strip())
+    query = f"INSERT INTO colors ([hex],[kind],[palte_id]) VALUES('{hex}','{kind}', {palte_id});"
+    update_query(query)
+
+
+def create_palte(project_id: str, palte_name: str):
+    project_id = int(project_id.strip())
+    query = f"INSERT INTO paltes ([project_id],[palte_name]) VALUES({project_id},'{palte_name}');"
+    update_query(query)
+
+
+def get_palte(palte_id: str):
+    palte_id = int(palte_id.strip())
+    query = f"SELECT * FROM paltes WHERE palte_id={palte_id};"
+    return select_one_query(query)
+
+
+def get_colors_by_plate(palte_id: str):
+    palte_id = int(palte_id.strip())
+    query = f"SELECT hex,kind FROM colors WHERE palte_id={palte_id};"
+    return select_all_query(query)
+
+
+def get_animations_by_kind(kind: str):
+    kind = kind.strip()
+    query = f"SELECT [animation_url],[animation_name] FROM animations WHERE animation_kind='{kind}';"
+    return select_all_query(query)
+
+
+def get_animations_by_template(template_id: str):
+    template_id = int(template_id.strip())
+    query = f"SELECT animation_url,animation_name,animation_kind FROM animations WHERE template_id={template_id};"
+    return select_all_query(query)
+
+
+def new_doc(project_id: int, doc_url: str, doc_name:str):
+    query = f"INSERT INTO docs([project_id], [doc_url], [doc_name]) VALUES({project_id}, '{doc_url}', '{doc_name}');"
+    update_query(query)
+
 
 
 def change_animation(path):
@@ -263,7 +548,5 @@ def change_animation(path):
     pass
 
 
-
 if __name__ == '__main__':
     application.run()
-
---
+++ db.py
@@ -0,0 +1,14 @@
+# from flaskext.mysql import MySQL
+#
+#
+# def select_statement(table_name, columns_name, condition_statement):
+#     execute_statement()
+#
+# def execute_statement(mysql: MySQL, statement: str):
+#     cursor = mysql.get_db().cursor()
+#     con = mysql.get_db()
+#     result = cursor.execute(statement)
+#     con.commit()
+#     cursor.close()
+#
+#     return result
